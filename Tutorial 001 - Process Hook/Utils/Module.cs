aca60130a303dbefa1e26369e81b4472e872c586
martynasapanavicius <martynas.apanavicius@gmail.com>
2019-07-20 15:59:53 +0300

    Tutorial 001 - ProcessModule wrapper

---
+++ Tutorial 001 - Process Hook/Utils/Module.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace RCi.Tutorials.Csgo.Cheat.External.Utils
+{
+    /// <summary>
+    /// Wrapper for <see cref="ProcessModule"/>.
+    /// </summary>
+    public class Module :
+        IDisposable
+    {
+        #region // storage
+
+        /// <inheritdoc cref="Process"/>
+        private Process Process { get; set; }
+
+        /// <inheritdoc cref="ProcessModule"/>
+        private ProcessModule ProcessModule { get; set; }
+
+        #endregion
+
+        #region // ctor
+
+        /// <summary />
+        public Module(Process process, ProcessModule processModule)
+        {
+            Process = process;
+            ProcessModule = processModule;
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            Process.Dispose();
+            Process = default;
+
+            ProcessModule.Dispose();
+            ProcessModule = default;
+        }
+
+        #endregion
+    }
+}
