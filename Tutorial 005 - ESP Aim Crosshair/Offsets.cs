4d4b30da3a45ceb7edf93fab2f7e8510e34e12e0
martynasapanavicius <martynas.apanavicius@gmail.com>
2019-07-25 23:25:21 +0300

    Read offsets from file.

--- Tutorial 005 - ESP Aim Crosshair/Offsets.cs
+++ Tutorial 005 - ESP Aim Crosshair/Offsets.cs
@@ -7,12 +7,58 @@
     {
         public static float weapon_recoil_scale = 2.0f;
 
-        public static int dwLocalPlayer = 0xCF3A3C;
-        public static int dwClientState = 0x58CCFC;
-        public static int dwClientState_ViewAngles = 0x4D88;
-        public static int m_vecOrigin = 0x138;
-        public static int m_vecViewOffset = 0x108;
-        public static int m_aimPunchAngle = 0x302C;
-        public static int m_iFOV = 0x31E4;
+        public static int dwLocalPlayer;
+        public static int dwClientState;
+        public static int dwClientState_ViewAngles;
+        public static int m_vecOrigin;
+        public static int m_vecViewOffset;
+        public static int m_aimPunchAngle;
+        public static int m_iFOV;
+
+        /// <summary />
+        static Offsets()
+        {
+            #region // find offsets file
+
+            const string OFFSETS_FILENAME = "offsets.txt";
+            var directoryInfo = new System.IO.DirectoryInfo(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location));
+            System.IO.FileInfo fileInfo;
+            do
+            {
+                fileInfo = System.Linq.Enumerable.FirstOrDefault(directoryInfo.GetFiles(), fi => string.Equals(fi.Name, OFFSETS_FILENAME));
+                directoryInfo = directoryInfo.Parent;
+            } while (fileInfo is null && !(directoryInfo is null));
+            if (fileInfo is null)
+            {
+                throw new System.IO.FileNotFoundException($"Cannot find '{OFFSETS_FILENAME}'.");
+            }
+
+            #endregion
+
+            #region // parse file and set values
+
+            var fieldInfos = typeof(Offsets).GetFields();
+            foreach (var line in System.IO.File.ReadAllLines(fileInfo.FullName))
+            {
+                var match = System.Text.RegularExpressions.Regex.Match(line, @"\A(?<name>.+) = (?<value>.+)\z");
+                if (!match.Success)
+                {
+                    continue;
+                }
+
+                var fieldValueStr = match.Groups["value"].Value;
+                if (!int.TryParse(fieldValueStr, out var fieldValue) &&
+                    !int.TryParse(fieldValueStr.Substring(2, fieldValueStr.Length - 2), System.Globalization.NumberStyles.HexNumber, null, out fieldValue))
+                {
+                    continue;
+                }
+
+                // find corresponding field and set value
+                var fieldInfo = System.Linq.Enumerable.FirstOrDefault(fieldInfos, fi => string.Equals(fi.Name, match.Groups["name"].Value) && fi.FieldType == typeof(int));
+                fieldInfo?.SetValue(default, fieldValue);
+            }
+
+            #endregion
+        }
     }
 }
