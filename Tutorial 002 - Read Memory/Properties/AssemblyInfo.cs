889ab6199a688872b2f50396bd0c40e500cd217c
martynasapanavicius <martynas.apanavicius@gmail.com>
2019-07-29 01:46:17 +0300

    AssemblyInfo.cs update

--- Tutorial 002 - Read Memory/Properties/AssemblyInfo.cs
+++ Tutorial 002 - Read Memory/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tutorial 001 - Process Hook")]
+[assembly: AssemblyTitle("RCi.Tutorials.Csgo.Cheat.External")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tutorial 001 - Process Hook")]
+[assembly: AssemblyProduct("RCi.Tutorials.Csgo.Cheat.External")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
